25b3029670fc6d69dd05abad5c349e9ce18b3ccb
yujie <samyujie@gmail.com>
2026-05-23 01:54:51 +0800

    up

--- dist/pchat.js
+++ dist/pchat.js
@@ -5038,6 +5038,7 @@ const ChatApp = {
             if (!this.currentMessages.find(m => m.id === msg.id)) this.currentMessages.push(msg);
         }
         this._appendMsgRaw(document.getElementById("message-list"), msg);
+        this._scroll();
     },
 
     // ---- Transfer progress UI ----
